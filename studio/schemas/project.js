ca51939e8ac953adca38928a0a08ea4a87c2173b
Lucien Siani <luciensiani@gmail.com>
2021-11-09 12:24:52 +0100

    creating new schemas

---
+++ studio/schemas/project.js
@@ -0,0 +1,40 @@
+export default {
+  name: "project",
+  type: "document",
+  title: "Project",
+  fields: [
+    {
+      name: "tilte",
+      type: "string",
+      title: "Title",
+    },
+
+    {
+      name: "description",
+      type: "string",
+      title: "description",
+    },
+
+    {
+      name: "link",
+      type: "string",
+      title: "Link",
+    },
+
+    {
+      name: "image",
+      type: "image",
+      title: "Image",
+      options: {
+        hotspot: true,
+      },
+    },
+
+    {
+      name: "categories",
+      title: "Categories",
+      type: "array",
+      of: [{ type: "reference", to: { type: "category" } }],
+    },
+  ],
+};
